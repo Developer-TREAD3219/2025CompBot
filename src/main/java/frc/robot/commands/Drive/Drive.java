330e46719748ddb3ed97f79739c442ab7199472a
TREAD 3219 <Tread3219@gmail.com>
2025-01-21 16:26:42 -0800

    2025 Comp Bot - basic Rev MAXSwerve code and Colby's LimeLight code from 2024 Comp Bot

---
+++ src/main/java/frc/robot/commands/Drive/Drive.java
@@ -0,0 +1,2 @@
+package frc.robot.commands.Drive;
+//a lot of work,driving
---
+++ src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
@@ -0,0 +1,75 @@
+package frc.robot.subsystems;
+
+// Currently not used
+//import org.opencv.photo.Photo;
+import org.photonvision.PhotonCamera;
+import org.photonvision.common.hardware.VisionLEDMode;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.math.geometry.Transform3d;
+// Currently not used
+// import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.VisionConstants;
+
+public class LimeLightSubsystem extends SubsystemBase {
+    //limelight for reading Apriltags 
+    PhotonCamera camera;
+    public PhotonPipelineResult result;
+    public PhotonTrackedTarget bestTarget;
+
+    public LimeLightSubsystem() {
+        camera = new PhotonCamera(VisionConstants.kCameraName);
+    }
+
+    public void driverMode() {
+        camera.setDriverMode(true);
+        camera.setLED(VisionLEDMode.kOff);
+    }
+
+    public void dectectAprilTags(){
+        camera.setDriverMode(false);
+        camera.setPipelineIndex(0);
+        camera.setLED(VisionLEDMode.kOff);
+    }
+
+    public void update() {
+        result = camera.getLatestResult();
+    }
+    public void findBestTarget(){
+        bestTarget = result.getBestTarget();
+    }
+    public int getApriltagID(){
+        return bestTarget.getFiducialId();
+    }
+    public double getSkew(){
+        return bestTarget.getSkew();
+    }
+    public double getYaw(){
+        return bestTarget.getYaw();
+    }
+    public double getPitch(){
+        return bestTarget.getPitch();
+    }
+    public boolean isAmbiguesPose(){
+        return bestTarget.getPoseAmbiguity() > 0.1;
+    }
+    public Transform3d getBestCameraToTarget(){
+        return bestTarget.getBestCameraToTarget();
+    }
+    @Override
+    public void periodic(){
+        update();
+        if (result.hasTargets()){
+            findBestTarget();
+            SmartDashboard.putNumber("Apriltag ID", getApriltagID());
+            SmartDashboard.putNumber("Skew", getSkew());
+            SmartDashboard.putNumber("Yaw", getYaw());
+            SmartDashboard.putNumber("Pitch", getPitch());
+            SmartDashboard.putBoolean("Ambigues Pose", isAmbiguesPose());
+        }
+        //This method will be called once per scheduler run
+    }
+}
