ec73d1452d402e56542d62fc7e76ada8143e61dd
TREAD 3219 <Tread3219@gmail.com>
2025-01-25 17:06:30 -0800

    Teaching Ezra

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,6 +1,7 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+// TODO: Finish the robot and party
 
 package frc.robot;
 
