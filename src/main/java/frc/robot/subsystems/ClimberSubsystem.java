5c5c7bb85e21762f785694dff633d39e8cec7126
TREAD 3219 <Tread3219@gmail.com>
2025-01-28 15:18:27 -0800

    Monday updates

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,6 +1,27 @@
 package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
     // TODO: ClimberSubsystem should extend from SubsystemBase. See an example subsystem here https://docs.wpilib.org/en/stable/docs/software/commandbased/subsystems.html
     // TODO:  Make placeholder extend and retract methods for the climber
-    public class ClimberSubsystem {
+    public class ClimberSubsystem extends SubsystemBase {
+            public ClimberSubsystem() {
+                
+
+                
+
+
+            }
+
+         public void Extend() {
+                    // code for extending Climber
+                }
+
     
+                public void retract(){
+                    // code for retracting Climber
+
+        
+                }
+                public void periodic() {
+                    // This method will be called once per scheduler run   
+         };
 }
