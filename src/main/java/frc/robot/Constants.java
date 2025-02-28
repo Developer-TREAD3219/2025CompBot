f1b87c1d2d300aac945173a47265f9e1130ba13a
TREAD 3219 <Tread3219@gmail.com>
2025-02-28 15:07:38 -0800

    Updated constants to match Cranberry Alarm

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -132,23 +132,22 @@ public final class Constants {
     public static final double kL3 = 24.3;
     public static final double kL4 = 48.7;
     public static final int kLimitSwitchPort = 1;
-    public static final double kMaxVelocity = 0.1;
-    public static final double kMaxAcceleration = 0.1;
+    public static final double kMaxVelocity = 65;
+    public static final double kMaxAcceleration = 200;
     public static final double kP = 0.1;
     public static final double kI = 0.0;
     public static final double kD = 0.0;
     public static final double kIZone = 5.0;
     public static final int kMaxCurrent = 60;
-    //TODO: We made this \/ a double maybe that was cringe
-    public static final double kCountsPerInch = 0.276987;
-    public static final double kMaxPos = 66.0;
-    public static final double kMax_output = 1;
-    public static final double kBottomPos = 0;
-    public static final double kPosTolerance = 0.5;
-    public static final double kS = 0.0;
-    public static final double kG = 0.;
-    public static final double kV = 0.0;
-    public static final double kMinPos = 0;
+    // public static final double kCountsPerInch = 0.276987;
+    // public static final double kMaxPos = 66.0;
+    // public static final double kMax_output = 1;
+    // public static final double kBottomPos = 0;
+    // public static final double kPosTolerance = 0.5;
+    // public static final double kS = 0.0;
+    public static final double kG = 0.5;
+    // public static final double kV = 0.0;
+    // public static final double kMinPos = 0;
   }
 
     public static final class coralDeliveryConstants {
